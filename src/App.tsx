065123569cff85f010be387e868a4d7ba8104920
dev-viniciuss <dev.mvsc@gmail.com>
2021-02-24 15:59:58 -0300

    feat: implementando a barra de experiência na page App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
-
 import GlobalStyles from './styles/global';
 
-const App: React.FC = () => {
+import ExperienceBar from './components/ExperienceBar';
+
+const App = () => {
   return (
     <>
-      <h1>Hello World</h1>
+      <div className="container">
+        <ExperienceBar />
+      </ div>
       <GlobalStyles />
     </>
   );
